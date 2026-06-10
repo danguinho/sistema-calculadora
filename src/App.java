abc82555854e871de0c7a1436ee32481b88b9d00
Daniel Moure Monteiro <daniel.moure72@gmail.com>
2026-06-10 20:31:52 -0300

    Implementando lógica de loop no cálculo de expressões

--- src/App.java
+++ src/App.java
@@ -9,38 +9,12 @@ public class App {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        boolean sistemaAtivo = false;
 
         Calculadora calc = new Calculadora();
 
-        double valora = 0;
-        double valorb = 0;
+        calc.iniciarSistema();
+        calc.fecharSistema();
 
-
-        do {
-
-            calc.iniciarSistema();
-
-            valora = sc.nextDouble();
-            Operacao op = Operacao.converterSinal(sc.next());
-            valorb = sc.nextDouble();
-            double resultado = calc.calcular(valora,op,valorb);
-
-            while(true){
-                String entrada = sc.next();
-                if(entrada.equals("=")){
-                    System.out.println(resultado);
-                    break;
-                } else{
-                    op = Operacao.converterSinal(entrada);
-                    valorb = sc.nextDouble();
-                    resultado = calc.calcular(resultado, op, valorb);
-                }
-            }
-
-            sistemaAtivo = calc.continuar();
-
-        } while (!sistemaAtivo);
-
+        sc.close();
     }
 }
--- src/Calculadora.java
+++ src/Calculadora.java
@@ -10,7 +10,44 @@ public class Calculadora {
         System.out.println("-----CALCULADORA------");
         System.out.println("Bem Vindo!");
         System.out.println("Digite uma expressão de soma, subtração, multiplicação ou divisão: ");
-        System.out.println("Não esqueça do espaço entre os operadores e os números na equação.");
+
+        boolean sistemaAtivo = false;
+        Operacao op;
+
+        /// Entrada do usuário:
+        String entrada = sc.nextLine();
+        String[] partes = entrada.split("(?<=[-+*/])|(?=[-+*/])");
+
+        /// Imprimindo a expressão:
+        for (String p : partes) {
+            System.out.print("(" + p + ")");
+        }
+
+        /// Convertendo os valores tipo String em operador e número decimal.
+        /// calculando o resultado da expressão e depois imprimindo o resultado
+        double n1 = Double.parseDouble(partes[0]);
+        op = Operacao.converterSinal(partes[1]);
+        double n2 = Double.parseDouble(partes[2]);
+        double resultado = calcular(n1, op, n2);
+        System.out.printf(" %.2f ", resultado);
+
+        /// Iniciando o loop do calculo:
+        do {
+            entrada = sc.nextLine();
+            partes = entrada.split("(?<=[-+*/])|(?=[-+*/])");
+
+            if (entrada.equalsIgnoreCase("sair")) {
+                sistemaAtivo = true;
+            } else {
+                op = Operacao.converterSinal(partes[0]);
+                n2 = Double.parseDouble(partes[1]);
+                resultado = calcular(resultado, op, n2);
+                System.out.printf(" %.2f ", resultado);
+            }
+
+        } while (!sistemaAtivo);
+
+
     }
 
 
@@ -31,22 +68,9 @@ public class Calculadora {
     }
 
 
-    public boolean continuar() {
-        System.out.println("Deseja Continuar? (y|n)");
-        char resp = sc.next().charAt(0);
-        if (resp == 'n') {
-            System.out.println("\nFechando Programa!\n");
-            return true;
-        } else {
-            System.out.println("\nRetornando ao Menu de Escolhas...\n");
-            return false;
-        }
-
+    public void fecharSistema() {
+        System.out.println("Fechando Calculadora...");
     }
-
-
-
-
 
 
 }
